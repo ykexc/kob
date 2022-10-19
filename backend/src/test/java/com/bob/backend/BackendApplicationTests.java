501f7aed10e152cff77f1cdc01f7bb9b428b8391
rtxsrp <3479103351@qq.com>
2022-10-19 19:15:01 +0800

    创建后端

---
+++ backend/src/test/java/com/bob/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bob.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
