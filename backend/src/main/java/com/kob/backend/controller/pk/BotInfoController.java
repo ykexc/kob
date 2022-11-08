ec2fcc3cb350a12f48bb9a9dd2079eb21baae1b8
rtxsrp <3479103351@qq.com>
2022-11-08 16:15:40 +0800

    register and login

--- backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
+++ backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
@@ -1,18 +1,16 @@
-package com.kob.backend;
+package com.kob.backend.controller.pk;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/pk/")
 public class BotInfoController {
 
-    @RequestMapping("getbotinfo/")
+    @RequestMapping("/getbotinfo")
     public Map<String, String> getBotInfo() {
         Map<String, String> bot1 = new HashMap<>();
         bot1.put("name", "apple");
